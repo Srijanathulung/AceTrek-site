4a2d5ae23a67a429ed217470755de1ab70ac3409
srijana <srijana.thulung123@gmail.com>
2022-08-10 17:22:30 +0545

    advisor horizontal line is added

--- src/components/Advisor/TripAdvisor.js
+++ src/components/Advisor/TripAdvisor.js
@@ -3,12 +3,14 @@ import './TripAdvisor.css';
 
 const TripAdvisor = () => {
   return (
-    <div className=' container-fluid tripadvisor'>
-      <div className='row'>
-
-        <h3 className='heading_title'>TRIP ADVISOR</h3>
+    <div className=' tripadvisor '>
+      {/* <div className='row'> */}
+        <div className=' container-lg trip-advisor'>
+        <h3 className='advisor_title'><span> TRIP ADVISOR </span> </h3>
+        <p className='advisor'>We have been receiving Certificate of Excellence every year since 2012. In 2019, TripAdvisor included us<br/> into the “Hall of Fame”. This honor is given to only those business that have won Certificate of<br/> Excellence for five consecutive years. And, on 2020, we received Traveler’s Choice Award which is the<br/> new name for Certificate of Excellence Award.</p>
+        </div>
       </div>
-    </div>
+    // </div>
   )
 }
 
